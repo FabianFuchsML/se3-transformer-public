0221341a2805adc361e5a129122be7d11c3e8a22
Fabian Fuchs <fabian.b.fuchs@gmail.com>
2021-02-26 19:12:25 +0000

    Update nbody_flags.py

--- experiments/nbody/nbody_flags.py
+++ experiments/nbody/nbody_flags.py
@@ -41,7 +41,7 @@ def get_flags():
                                  'springs_infer'])
     # location of data for relational inference
     parser.add_argument('--ri_data', type=str, default='data_generation')
-    parser.add_argument('--data_str', type=str, default='5_nobox_mailvolker')
+    parser.add_argument('--data_str', type=str, default='my_datasetfile')
     # how many time steps to predict into the future
     parser.add_argument('--ri_delta_t', type=int, default=10)
     # how many time steps to cut off from dataset in the beginning
